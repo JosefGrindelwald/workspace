dd9905cfe8973d76b1e0a1c66fc8c83dbe530bfe
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-04 21:39:49 +0100

    Update main.py

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,6 +1,10 @@
 import pygame
+from constants.py import SCREEN_WIDTH
+from constants.py import SCREEN_HEIGHT
 def main():
     print("Starting Asteroids with pygame version: VERSION")
+    print(Screen width: 1280
+Screen height: 720)
 
 
 if __name__ == "__main__":
