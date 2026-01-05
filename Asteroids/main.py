56f91a0b10ac47727847ca73648fbeafb6074482
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-05 20:02:27 +0100

    Update main.py

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,6 +1,13 @@
 import pygame
+from logger import log_state
 from constants import *
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while(True):
+        log_state()
+        "black".fill
+        display.flip()
     print("Starting Asteroids with pygame version: VERSION")
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
