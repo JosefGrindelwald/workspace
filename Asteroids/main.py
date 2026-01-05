210ed2baeccee2cf49d3c1fe705bda8e75bbe9f6
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-05 20:54:04 +0100

    Update main.py

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,35 +1,27 @@
 import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-from constants import *
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Asteroids")
-
     clock = pygame.time.Clock()
     dt = 0
-    running = True
 
-
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
+    while True:
         log_state()
 
-        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill("black")
         pygame.display.flip()
 
-        # FPS begrenzen & Delta Time berechnen
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
-    pygame.quit()
 
 if __name__ == "__main__":
     main()
