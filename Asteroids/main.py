4d34b941a7e7e34c934cf6b31456cdd02714bf43
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-06 20:53:11 +0100

    Update main.py

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -9,7 +9,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
 
