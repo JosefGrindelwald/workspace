d17681e1e9008e644c731aafdbb5d43247b597cb
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-07 07:04:26 +0100

    Update shot.py

--- Asteroids/shot.py
+++ Asteroids/shot.py
@@ -2,7 +2,7 @@ import pygame
 from circleshape import *
 from constants import *
 
-class shot(CircleShape):
+class Shot(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
