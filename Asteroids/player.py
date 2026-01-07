379689f7b55d1b9907b5f128b1c87f182e7d1017
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-07 07:38:55 +0100

    Update player.py

--- Asteroids/player.py
+++ Asteroids/player.py
@@ -2,12 +2,13 @@ import pygame
 from circleshape import *
 from constants import *
 from shot import *
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        cooldown = 0
-        PLAYER_SHOOT_COOLDOWN_SECONDS = 0.3
+        self.cooldown = 0
+        self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
 
     def triangle(self):
         forward = pygame.Vector2(0, -1).rotate(self.rotation)
@@ -28,34 +29,31 @@ class Player(CircleShape):
         )
 
     def rotate(self, direction, dt):
-        # direction: +1 (links), -1 (rechts)
         self.rotation += direction * PLAYER_TURN_SPEED * dt
+
     def move(self, direction, dt):
-        unit_vector = pygame.Vector2(0, -1)
-        rotated_vector = unit_vector.rotate(self.rotation)
-        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
-        self.position += rotated_with_speed_vector
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * direction * dt
+
     def shoot(self):
         forward = pygame.Vector2(0, -1).rotate(self.rotation)
-
         shot_position = self.position + forward * self.radius
         Shot(shot_position.x, shot_position.y, forward)
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        self.cooldown -= dt
+        self.cooldown -= dt  # Cooldown runterzählen
 
         if keys[pygame.K_a]:
             self.rotate(-1, dt)
         if keys[pygame.K_d]:
             self.rotate(+1, dt)
-        if keys[pygame.K_s]:
-            self.move(-1, -dt)
         if keys[pygame.K_w]:
             self.move(+1, dt)
-        if keys[pygame.K_SPACE]:
-            if self.cooldown <= 0:
-                self.shoot()
-                self.cooldown = self.PLAYER_SHOOT_COOLDOWN_SECONDS
-            if self.cooldown > 0:
-                None
+        if keys[pygame.K_s]:
+            self.move(-1, dt)
+
+        if keys[pygame.K_SPACE] and self.cooldown <= 0:
+            self.shoot()
+            self.cooldown = self.shoot_cooldown
     
