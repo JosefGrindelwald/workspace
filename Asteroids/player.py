942b0170d92db06098b76b33f34e0ae2882c7d20
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-07 11:31:49 +0100

    Update player.py

--- Asteroids/player.py
+++ Asteroids/player.py
@@ -6,27 +6,32 @@ from shot import *
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
+
+        if Player.image is None:
+            Player.image = pygame.image.load(
+                "assets/Spaceship.png"
+            ).convert_alpha()
+
         self.rotation = 0
         self.cooldown = 0
         self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
 
-    def triangle(self):
-        forward = pygame.Vector2(0, -1).rotate(self.rotation)
-        right = forward.rotate(90) * self.radius / 1.5
-
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
-
-        return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(
-            screen,
-            "white",
-            self.triangle(),
-            LINE_WIDTH
+        size = int(self.radius * 2)
+
+        scaled_image = pygame.transform.scale(
+            Player.image,
+            (size, size)
         )
+
+        rotated_image = pygame.transform.rotate(
+            scaled_image,
+            self.rotation
+        )
+
+        rect = rotated_image.get_rect(center=self.position)
+        screen.blit(rotated_image, rect)
 
     def rotate(self, direction, dt):
         self.rotation += direction * PLAYER_TURN_SPEED * dt
