7dd40f4826c13022782b260b7a5202fda8eaddab
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-07 07:06:01 +0100

    Update main.py

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -13,9 +13,11 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable, shots)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
     while True:
