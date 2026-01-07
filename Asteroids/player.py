8f1ce9d22d61d8f1ebc92b9cddbb24ae15e69e96
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-07 07:13:02 +0100

    Update player.py

--- Asteroids/player.py
+++ Asteroids/player.py
@@ -1,7 +1,7 @@
 import pygame
 from circleshape import *
 from constants import *
-
+from shot import *
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
@@ -33,6 +33,11 @@ class Player(CircleShape):
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+    def shoot(self):
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
+
+        shot_position = self.position + forward * self.radius
+        Shot(shot_position.x, shot_position.y, forward)
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -44,4 +49,6 @@ class Player(CircleShape):
             self.move(-1, -dt)
         if keys[pygame.K_w]:
             self.move(+1, dt)
+        if keys[pygame.K_space]:
+            self.shoot
     
