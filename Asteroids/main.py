77f8827befe685ba6d0c3bb167b09f19667dcfba
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-07 06:25:20 +0100

    Update main.py

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -11,7 +11,9 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
    
     while True:
