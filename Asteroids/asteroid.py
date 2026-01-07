37b6e539e193e6781c51542aab6b6e26cd3286c9
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-07 08:20:25 +0100

    Update asteroid.py

--- Asteroids/asteroid.py
+++ Asteroids/asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 from circleshape import *
 from constants import *
+from logger import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -14,6 +16,18 @@ class Asteroid(CircleShape):
             self.radius,
             LINE_WIDTH
         )
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            for _ in range(2):
+                angle = random.uniform(20, 50)  
+                direction = pygame.Vector2(0, -1).rotate(angle)
+                position = self.position + direction * new_radius
+                asteroid = Asteroid(position.x, position.y, new_radius)
+                asteroid.velocity = direction* 1.2
 
     def update(self, dt):
         self.position += self.velocity * dt
