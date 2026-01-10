407b0c7dd12ee56fb37b87bff15c58ad0ae16a0a
JosefGrindelwald <james.t.nemo@gmail.com>
2026-01-10 16:41:06 +0100

    Update player.py

--- Asteroids/player.py
+++ Asteroids/player.py
@@ -19,7 +19,7 @@ class Player(CircleShape):
 
         self.rotation = 0
         self.cooldown = 0
-        self.player_level = 0
+        self.level = 1
         self.shoot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
 
 
