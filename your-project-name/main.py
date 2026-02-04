cae15070e1c53264616facca2ab3d07df46c2acb
JosefGrindelwald <james.t.nemo@gmail.com>
2026-02-04 23:11:45 +0100

    Update main.py

--- your-project-name/main.py
+++ your-project-name/main.py
@@ -2,7 +2,9 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
+messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 # Load environment variables
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -33,8 +35,7 @@ prompt = args.user_prompt
 
 # Call the Gemini API
 response = client.models.generate_content(
-    model="gemini-2.5-flash",           # oder "gemini-1.5-flash" je nach Verfügbarkeit 2026
-    contents=prompt
+    model="gemini-2.5-flash", contents=messages
 )
 
 # Ensure usage metadata exists
