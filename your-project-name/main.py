bc6c6162cc3bdb0ab7226acba854baaf8ea06ba1
JosefGrindelwald <james.t.nemo@gmail.com>
2026-02-12 07:40:19 +0100

    Update main.py

--- your-project-name/main.py
+++ your-project-name/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
-
+from call_function import available_functions
 # Load environment variables
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
